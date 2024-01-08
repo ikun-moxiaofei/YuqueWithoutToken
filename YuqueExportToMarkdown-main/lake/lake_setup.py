fa212ff335e39e915913bfdfbb96d79bac9faf9b
ikun-moxiaofei <1091909200@qq.com>
2024-01-09 00:18:39 +0800

    1.1

--- YuqueExportToMarkdown-main/lake/lake_setup.py
+++ YuqueExportToMarkdown-main/lake/lake_setup.py
@@ -4,8 +4,8 @@ import json
 import yaml
 import os
 
-from lake.lake_handle import MyParser, MyContext
-from lake.failure_result_parser import parse_failure_result
+from lake_handle import MyParser, MyContext
+from failure_result_parser import parse_failure_result
 
 # parent_id和book
 parent_id_and_child: dict = {}
